20aa263ec2683afd65387adbba2a39a58647f68b
Manasa Kandula <manasakandula.m@gmail.com>
2024-06-04 19:34:20 +0000

    added simple client

---
+++ deploy-server.sh
@@ -0,0 +1,15 @@
+# !bin/bash
+gcloud config set project $PROJECT_ID
+
+gcloud functions deploy server \
+  --gen2 \
+  --region=$LOCATION \
+  --runtime=python310 \
+  --source=./set2/server \
+  --entry-point=http_process_image\
+  --min-instances=0\
+  --trigger-http \
+  --memory=1GB \
+  --allow-unauthenticated \
+  --run-service-account=$FUNCTIONS_SERVICE_ACCOUNT \
+  --set-env-vars=PROJECT_ID=$PROJECT_ID,LOCATION=$LOCATION
